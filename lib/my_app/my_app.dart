662a69b6f8830d9af4476c3ad69770edb48b2027
abdallah <abdallhamohamed116@gmail.com>
2024-07-16 19:07:52 +0300

    responsive screen utils package

--- lib/my_app/my_app.dart
+++ lib/my_app/my_app.dart
@@ -2,6 +2,7 @@ import 'package:ecommerce_app/core/app/connectivity_controller.dart';
 import 'package:ecommerce_app/core/app/env_variables.dart';
 import 'package:ecommerce_app/core/common/screens/no_network_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class Buytopia_App extends StatelessWidget {
   const Buytopia_App({super.key});
@@ -12,27 +13,34 @@ class Buytopia_App extends StatelessWidget {
       valueListenable: ConnectivityController.instance.isConnected,
       builder: (_, value, child) {
         if (value) {
-          return MaterialApp(
-              debugShowCheckedModeBanner: EnvVariable.instance.debugMode,
-              // title: "Buytopia",
-              theme: ThemeData(
-                colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-                useMaterial3: true,
-              ),
-              builder: (context, widget) {
-                return Scaffold(
-                  body: Builder(
-                    builder: (context) {
-                      ConnectivityController.instance.init();
-                      return widget!;
-                    },
+          return ScreenUtilInit( 
+              designSize: const Size(390, 844),
+      minTextAdapt: true,
+      splitScreenMode: true,
+            builder: (_,child) {
+              return MaterialApp(
+                  debugShowCheckedModeBanner: EnvVariable.instance.debugMode,
+                  // title: "Buytopia",
+                  theme: ThemeData(
+                    colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+                    useMaterial3: true,
                   ),
-                );
-              },
-              home: Scaffold(
-                  appBar: AppBar(
-                title: const Text('Buytopia'),
-              )));
+                  builder: (context, widget) {
+                    return Scaffold(
+                      body: Builder(
+                        builder: (context) {
+                          ConnectivityController.instance.init();
+                          return widget!;
+                        },
+                      ),
+                    );
+                  },
+                  home: Scaffold(
+                      appBar: AppBar(
+                    title: const Text('Buytopia'),
+                  )));
+            },
+          );
         } else {
           return const MaterialApp(
             home: NoNetworkScreen(),
